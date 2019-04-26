8ecfdc4ec6e4641560ca4129156528d417f2a370
Isaac Iniongun <iniongunisaac@gmail.com>
2019-04-26 17:39:15 +0100

    Add photo selection

--- WhatFlower/ViewController.swift
+++ WhatFlower/ViewController.swift
@@ -8,13 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    
+    private var imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let userImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = userImage
+        } else {
+            print("Unable to get user image.")
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
     }
 
 
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        imagePicker.sourceType = .savedPhotosAlbum
+        imagePicker.allowsEditing = false
+        
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
 }
 
